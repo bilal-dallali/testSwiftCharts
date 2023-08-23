d18eaac1bc97ac65f752f48bdd858b7a54f7cb79
bilal-dallali <dallali-bilal@hotmail.fr>
2023-08-23 08:28:04 +0200

    Initial Commit

---
+++ chart/chartApp.swift
@@ -0,0 +1,17 @@
+//
+//  chartApp.swift
+//  chart
+//
+//  Created by Bilal Dallali on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct chartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
